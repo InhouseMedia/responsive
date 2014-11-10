635f2835f2717cb0ae11815bfd9f5023ae1a1391
Ramon Klanke <ramonklanke@hotmail.com>
2014-11-10 02:21:15 +0100

    Adding default datbase data

---
+++ Library/Config/Default_Data/dbo.AspNetUserRoles.data.sql
@@ -0,0 +1,4 @@
+DELETE FROM [dbo].[AspNetUserRoles]
+GO
+INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'ecec2056-053c-438e-8307-f2b8d73ec2dc', N'def2da07-2bf6-4b83-ac73-0abe4e89faf0')
+GO
