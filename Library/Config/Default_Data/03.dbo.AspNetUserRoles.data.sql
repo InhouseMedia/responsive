38e7ef69f2c703fd5979227fef6bd154ca15e247
Ramon Klanke <info@inhousemedia.com>
2014-12-24 00:05:26 +0100

    Add new user to standard config and database import: username: adrian.arsene@gmail.com password: @drianArsene01

--- Library/Config/Default_Data/03.dbo.AspNetUserRoles.data.sql
+++ Library/Config/Default_Data/03.dbo.AspNetUserRoles.data.sql
@@ -1,11 +1,12 @@
 DELETE FROM [dbo].[AspNetUserRoles]
 GO
-INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'2dffd82b-4f60-48cb-b9ab-d7e41e620e6e', N'67242414-4b4c-448a-bfe7-e50ccd94d9b5')
-INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'973141f0-d1db-497f-bdd1-07afa87f8a6f', N'67242414-4b4c-448a-bfe7-e50ccd94d9b5')
-INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'07a67c57-734d-457d-b5f8-f5a40a235fda', N'8ddbe24d-b474-452b-a887-37006f70e6ac')
-INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'2dffd82b-4f60-48cb-b9ab-d7e41e620e6e', N'90a5cd04-27d2-4b4b-b31d-991a22f7b4e0')
-INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'2dffd82b-4f60-48cb-b9ab-d7e41e620e6e', N'9c7efe73-83dd-4e89-ac8c-d40c6edb2d0b')
-INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'2dffd82b-4f60-48cb-b9ab-d7e41e620e6e', N'ac0ab6e1-76fe-43a4-9b1f-7b62a1b11921')
-INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'973141f0-d1db-497f-bdd1-07afa87f8a6f', N'ac0ab6e1-76fe-43a4-9b1f-7b62a1b11921')
-INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'ecec2056-053c-438e-8307-f2b8d73ec2dc', N'def2da07-2bf6-4b83-ac73-0abe4e89faf0')
+INSERT INTO [dbo].[AspNetUserRoles] ([RoleId], [UserId]) VALUES (N'8ddbe24d-b474-452b-a887-37006f70e6ac', N'07a67c57-734d-457d-b5f8-f5a40a235fda')
+INSERT INTO [dbo].[AspNetUserRoles] ([RoleId], [UserId]) VALUES (N'def2da07-2bf6-4b83-ac73-0abe4e89faf0', N'168f53a2-b257-4efd-8c71-eeb8728a9093')
+INSERT INTO [dbo].[AspNetUserRoles] ([RoleId], [UserId]) VALUES (N'67242414-4b4c-448a-bfe7-e50ccd94d9b5', N'2dffd82b-4f60-48cb-b9ab-d7e41e620e6e')
+INSERT INTO [dbo].[AspNetUserRoles] ([RoleId], [UserId]) VALUES (N'90a5cd04-27d2-4b4b-b31d-991a22f7b4e0', N'2dffd82b-4f60-48cb-b9ab-d7e41e620e6e')
+INSERT INTO [dbo].[AspNetUserRoles] ([RoleId], [UserId]) VALUES (N'9c7efe73-83dd-4e89-ac8c-d40c6edb2d0b', N'2dffd82b-4f60-48cb-b9ab-d7e41e620e6e')
+INSERT INTO [dbo].[AspNetUserRoles] ([RoleId], [UserId]) VALUES (N'ac0ab6e1-76fe-43a4-9b1f-7b62a1b11921', N'2dffd82b-4f60-48cb-b9ab-d7e41e620e6e')
+INSERT INTO [dbo].[AspNetUserRoles] ([RoleId], [UserId]) VALUES (N'67242414-4b4c-448a-bfe7-e50ccd94d9b5', N'973141f0-d1db-497f-bdd1-07afa87f8a6f')
+INSERT INTO [dbo].[AspNetUserRoles] ([RoleId], [UserId]) VALUES (N'ac0ab6e1-76fe-43a4-9b1f-7b62a1b11921', N'973141f0-d1db-497f-bdd1-07afa87f8a6f')
+INSERT INTO [dbo].[AspNetUserRoles] ([RoleId], [UserId]) VALUES (N'def2da07-2bf6-4b83-ac73-0abe4e89faf0', N'ecec2056-053c-438e-8307-f2b8d73ec2dc')
 GO
