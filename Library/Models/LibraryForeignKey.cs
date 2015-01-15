bed735949a0d91b95e384849ddb8e425ee070433
Ramon Klanke <info@inhousemedia.com>
2015-01-15 07:05:56 +0100

    Fix for documents that are uploaded also a changed the names of the model for documents so that it is more in line with other codenames

---
+++ Library/Models/LibraryForeignKey.cs
@@ -0,0 +1,35 @@
+﻿namespace Library.Models
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.ComponentModel.DataAnnotations;
+	using System.ComponentModel.DataAnnotations.Schema;
+	using System.Data.Entity.ModelConfiguration.Conventions;
+	using System.Data.Entity;
+	using Microsoft.AspNet.Identity.EntityFramework;
+
+	class LibraryForeignKey
+	{
+	}
+
+	public partial class Article_PublishLogs
+	{
+		/*
+		public Article_PublishLogs()
+		{
+			this.ApplicationUsers = new HashSet<ApplicationUser>();
+			
+		}
+		//[ForeignKey("Published_By")]
+		public virtual ICollection<ApplicationUser> ApplicationUsers { get; set; }
+		
+		[ForeignKey("Published_By")]
+		public virtual ApplicationUser User { get; set; }
+		 */
+	}
+
+
+}
---
+++ Library/Models/LibraryModelMetadata.cs
@@ -0,0 +1,71 @@
+﻿namespace Library.Models
+{
+	using System;
+	using System.ComponentModel.DataAnnotations;
+	using System.ComponentModel.DataAnnotations.Schema;
+
+	using System.Data.Entity.ModelConfiguration.Conventions;
+	using System.Data.Entity;
+	using Microsoft.AspNet.Identity.EntityFramework;
+
+	using Library.Resources;
+
+	public class Article_Datanotation
+	{
+		[Range(0, 2)]
+		[Display(Name = "WebgridActive", ResourceType = typeof(Translate))]
+		public byte Active;
+
+		[DisplayFormat(DataFormatString = "{0:g}")]
+		[Display(Name = "WebgridCreatedBy", ResourceType = typeof(Translate))]
+		public string Created_By;
+
+		[DisplayFormat(DataFormatString = "{0:g}")]
+		[Display(Name = "WebgridCreationDate", ResourceType = typeof(Translate))]
+		public DateTime Creation_Date;
+	}
+
+	public class Article_Metadata_Datanotation
+	{
+		[StringLength(50)]
+		[Display(Name = "WebgridMetaTitle", Prompt = "WebgridMetaTitleInfo", ResourceType = typeof(Translate))]
+		public string Meta_Title;
+
+		[StringLength(50)]
+		[Display(Name = "WebgridKeywords", Prompt = "WebgridKeywordsInfo", ResourceType = typeof(Translate))]
+		public string Meta_Keywords;
+
+		[StringLength(50)]
+		[Display(Name = "WebgridMetaDescription", Prompt = "WebgridMetaDescriptionInfo", ResourceType = typeof(Translate))]
+		public string Meta_Description;
+	}
+
+	public class Article_Content_Datanotation
+	{
+		//[Range(0, 4)]
+		//public Nullable<decimal> Grade;
+	}
+
+	public class Article_PublishLogs_Datanotation
+	{
+		//[Key, ForeignKey("User")]
+		[Display(Name = "WebgridPublishedBy", ResourceType = typeof(Translate))]
+		public string Published_By;
+
+		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:g}")]
+		[Display(Name = "WebgridPublishedDate", ResourceType = typeof(Translate))]
+		public DateTime Published_Date;
+	}
+
+	[MetadataType(typeof(Article_Datanotation))]
+	public partial class Article { }
+
+	[MetadataType(typeof(Article_Metadata_Datanotation))]
+	public partial class Article_Metadata { }
+
+	//[MetadataType(typeof(Article_Content_Datanotation))]
+	//public partial class Article_Content { }
+
+	[MetadataType(typeof(Article_PublishLogs_Datanotation))]
+	public partial class Article_PublishLogs { }
+}
