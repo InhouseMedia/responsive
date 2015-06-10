dae0b6d7517e4974a0f2092b983f41d6ff6e1b80
Ramon Klanke <info@inhousemedia.com>
2015-06-10 23:09:58 +0200

    refactoring

--- Library/Helpers/StringHelper.cs
+++ Library/Helpers/StringHelper.cs
@@ -1,12 +1,5 @@
 ﻿namespace Library.Helpers
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Globalization;
-	using System.Text;
-	using System.Threading.Tasks;
-
 	public static class ExtensionMethods
 	{
 		public static string ToCapitalize(this string text)
@@ -16,14 +9,10 @@
 				return text;
 			}
 
-			//return CultureInfo.InvariantCulture.TextInfo.ToTitleCase(self);
 			return string.Format(
 					"{0}{1}",
 					text.Substring(0, 1).ToUpper(),
 					text.Substring(1));
-
 		}
-	}
-
-	
+	}	
 }
